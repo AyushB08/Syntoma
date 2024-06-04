828264b4ce51a6e1c1fee132e4acf56cf81a5163
AyushB08 <ayushbheemaiah@gmail.com>
2024-06-04 16:31:13 -0700

    uploadthing api

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+
 import { useAuth } from "@/contexts/authContext";
 import { useRouter } from "next/navigation";
-
 
 const Profile = () => {
   const router = useRouter();
   const authContext = useAuth();
+
+ 
+  if (!authContext || !authContext.user) {
+    router.push("/");
+    return null; 
+  }
 
   const handleLogout = () => {
     authContext.logout();
@@ -15,21 +21,19 @@ const Profile = () => {
 
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center">
-      {authContext && authContext.user && (
-        <div className="w-3/5 h-full flex flex-col items-center justify-center">
-          <h1 className="text-8xl text-center mb-10">
-            Welcome to your profile,{" "}
-            <span className="text-red-700">{authContext.user.username}</span>.
-          </h1>
-          
-          <button
-            onClick={handleLogout}
-            className="px-5 py-2 bg-red-700 rounded-lg hover:bg-red-900"
-          >
-            Sign Out
-          </button>
-        </div>
-      )}
+      <div className="w-3/5 h-full flex flex-col items-center justify-center">
+        <h1 className="text-8xl text-center mb-10">
+          Welcome to your profile,{" "}
+          <span className="text-red-700">{authContext.user.username}</span>.
+        </h1>
+        
+        <button
+          onClick={handleLogout}
+          className="px-5 py-2 bg-red-700 rounded-lg hover:bg-red-900"
+        >
+          Sign Out
+        </button>
+      </div>
     </div>
   );
 };
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
 
 const Navbar = () => {
+  const links = [
+    { href: "/", label: "Home" },
+    { href: "/sign-in", label: "Sign In" },
+    { href: "/sign-up", label: "Sign Up" },
+    { href: "/upload", label: "Upload" },
+  ];
+
   return (
     <nav className="absolute top-0 left-0 w-full">
       <div className="container mx-auto flex justify-center">
         <ul className="items-center flex flex-row justify-around p-4 text-lg font-bold text-white w-1/3">
-          <Link href="/">Home</Link>
-          <Link href="/sign-in">Sign In</Link>
-          <Link href="/sign-up">Sign Up</Link>
+          
+            
+
+          
+          
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
