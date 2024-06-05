11ca837a5ce6e5fec03f059b44f8d232c2da29e2
AyushB08 <ayushbheemaiah@gmail.com>
2024-06-04 19:04:07 -0700

    move uploadthing to new branch

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,27 +1,13 @@
 import Link from "next/link";
 
 const Navbar = () => {
-  const links = [
-    { href: "/", label: "Home" },
-    { href: "/sign-in", label: "Sign In" },
-    { href: "/sign-up", label: "Sign Up" },
-    { href: "/upload", label: "Upload" },
-  ];
-
   return (
     <nav className="absolute top-0 left-0 w-full">
       <div className="container mx-auto flex justify-center">
         <ul className="items-center flex flex-row justify-around p-4 text-lg font-bold text-white w-1/3">
-          
-            
-
-          
-          
-          {links.map((link) => (
-            <li key={link.href}>
-              <Link href={link.href}>{link.label}</Link>
-            </li>
-          ))}
+          <Link href="/">Home</Link>
+          <Link href="/sign-in">Sign In</Link>
+          <Link href="/sign-up">Sign Up</Link>
         </ul>
       </div>
     </nav>
