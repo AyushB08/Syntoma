7be7ca5ec8346459347b0158b63f07fbb545c3f1
AyushB08 <ayushbheemaiah@gmail.com>
2024-06-05 19:37:47 -0700

    hosted postgreSQL db with neon.tech

--- server/index.js
+++ server/index.js
@@ -1,61 +1,53 @@
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
-
-
-
 app.use(cors());
 app.use(express.json());
 
-
-app.post("/sign-up", async(req, res) => {
+app.post("/sign-up", async (req, res) => {
     try {
-        const {email, username, password} = req.body;
+        const { email, username, password } = req.body;
 
         if (!email || !username || !password) {
-            return res.status(400).json({ error: "Please provide email, username, and password"})
+            return res.status(400).json({ error: "Please provide email, username, and password" });
         }
 
-        const newUser = await pool.query("INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING *", 
-        [email, username, password]);
-        
+        const newUser = await pool.query(
+            "INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING *", 
+            [email, username, password]
+        );
 
         res.json(newUser.rows[0]);
-       
-    }
-    catch (error) {
-
-    }
-});
-
-
-
-app.post("/sign-in", async (req, res) => {
-    try {
-    const { email, password } = req.body;
-
-    
-    const user = await pool.query("SELECT * FROM users WHERE email = $1 AND password = $2", [email, password]);
-    
-    if (user.rows.length === 1) {
-        const username = user.rows[0].username;
-       
-        res.json({ username });
-        
-    } else {
-       
-        res.status(401).json({ error: "Invalid username or password" });
-    }
     } catch (error) {
         console.error(error.message);
         res.status(500).json({ error: "Server Error" });
     }
 });
 
-app.post("/save-image", (req, res) => {
+app.post("/sign-in", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await pool.query(
+            "SELECT * FROM users WHERE email = $1 AND password = $2", 
+            [email, password]
+        );
+
+        if (user.rows.length === 1) {
+            const username = user.rows[0].username;
+            res.json({ username });
+        } else {
+            res.status(401).json({ error: "Invalid email or password" });
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+app.post("/save-image", async (req, res) => {
     try {
         const { username, fileurl } = req.body;
 
@@ -63,7 +55,7 @@ app.post("/save-image", (req, res) => {
             return res.status(400).json({ error: "Please provide username and file URL" });
         }
 
-        const newImage = pool.query(
+        const newImage = await pool.query(
             "INSERT INTO images (username, fileurl) VALUES ($1, $2) RETURNING *",
             [username, fileurl]
         );
@@ -74,7 +66,6 @@ app.post("/save-image", (req, res) => {
         res.status(500).json({ error: "Server Error" });
     }
 });
-
 
 app.listen(8000, () => {
     console.log("Server has started on port 8000");
