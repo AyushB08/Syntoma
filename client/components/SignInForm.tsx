e2c3c8fe04444efe1d7e1bc7b9e3211216475d00
AyushB08 <ayushbheemaiah@gmail.com>
2024-06-09 08:09:12 +0530

    styles

--- client/components/SignInForm.tsx
+++ client/components/SignInForm.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/contexts/authContext";
+import Link from "next/link";
 
 export default function SignInForm() {
   const [email, setEmail] = useState("");
@@ -40,17 +41,17 @@ export default function SignInForm() {
 
   return (
     <form className="w-1/2 h-auto rounded-lg bg-white p-6 shadow-lg " onSubmit={onSubmitForm}>
-      <h1 className="flex flex-row text-4xl mb-4 justify-center items-center text-black ">Company</h1>
+      <h1 className="text-blue-700 font-bold flex flex-row text-4xl mb-4 justify-center items-center  ">Vital</h1>
       <div className="mb-4">
         <label htmlFor="email" className="text-black text-sm font-semibold mb-1">Email Address</label>
-        <input onChange={e => setEmail(e.target.value)} value={email} type="email" id="email" className="text-black w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-700" placeholder="Enter your email" />
+        <input onChange={e => setEmail(e.target.value)} value={email} type="email" id="email" className="text-black w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-600" placeholder="Enter your email" />
       </div>
       <div className="mb-6 text-black">
         <label htmlFor="password" className="text-black text-sm font-semibold mb-1">Password</label>
-        <input onChange={e => setPassword(e.target.value)} value={password} type="password" id="password" className="text-black w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-700" placeholder="Enter your password" />
+        <input onChange={e => setPassword(e.target.value)} value={password} type="password" id="password" className="text-black w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-600" placeholder="Enter your password" />
       </div>
-      <button type="submit" className="w-full bg-red-700 text-white py-2 rounded-lg hover:bg-red-900 focus:outline-none">Sign In</button>
-      <p className="mt-4 text-blue-700 flex  items-center">Sign Up &rarr;</p>
+      <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 focus:outline-none">Sign In</button>
+      <Link href="/sign-up" className="mt-4 text-blue-600 hover:text-black flex  items-center">Sign Up &rarr;</Link>
     </form>
   );
 }
