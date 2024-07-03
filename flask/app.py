4c1730496964234dfb81ad4cde2b8e500d293bd1
Jainish Patel <jaicode2020@gmail.com>
2024-07-03 01:46:22 -0400

    knee model FINAL DONE!
    used a slightly modified efficientb5 model to predict knee

--- flask/app.py
+++ flask/app.py
@@ -1,17 +1,56 @@
 import requests
 import urllib.parse
-import torch
-import torchvision.transforms as transforms
 from PIL import Image
 from io import BytesIO
 from ultralytics import YOLO
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from keras._tf_keras.keras.applications import EfficientNetB5
+from keras._tf_keras.keras.models import Model
+from keras._tf_keras.keras.layers import BatchNormalization, Dense, Dropout
+from keras import regularizers
+import tensorflow as tf
+from keras import backend as K
+from keras._tf_keras.keras.applications.efficientnet import preprocess_input
+import numpy as np
+from PIL import Image
+
+def build_model(num_classes, img_shape, weights_path):
+    K.clear_session()
+    base_model = tf.keras.applications.EfficientNetB5(include_top = False, weights = weights_path, input_shape=img_shape, pooling = 'max')
+    base_model.trainable = True
+    x = base_model.output
+    x = BatchNormalization(axis = -1, momentum = 0.99, epsilon = 0.001)(x)
+    x = Dense(256, kernel_regularizer = regularizers.l2(0.016), activity_regularizer = regularizers.l1(0.006), bias_regularizer = regularizers.l1(0.006), activation = 'relu')(x)
+    x = Dropout(rate = .4, seed = 123)(x)
+    output = Dense(num_classes, activation = 'softmax')(x)
+    model = Model(inputs = base_model.input, outputs = output)
+    return model
+
+img_size = (224, 224)
+img_shape = (img_size[0], img_size[1], 3)
+num_classes = 3
+weights_path = 'models/knee_model/efficientnetb5_notop.h5'
+
+new_model = build_model(num_classes, img_shape, weights_path)
+new_model.load_weights('models/knee_model/model_weights.h5')
+new_model.compile(optimizer = 'Adamax', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+def preprocess_pillow_image(pil_img, img_size):
+    img = pil_img.resize(img_size)
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis = 0)
+    img_array = preprocess_input(img_array)
+    return img_array
+
+def predict_classes(pil_img):
+    img_array = preprocess_pillow_image(pil_img, img_size)
+    predictions = new_model.predict(img_array)
+    return predictions.tolist()
 
 app = Flask(__name__)
 CORS(app)
 knee_crop_model = YOLO("models/knee_crop.pt")
-knee_classification_model = YOLO("models/knee_classification.pt")
 
 @app.route("/process_knee")
 def process_knee_scan():
@@ -20,30 +59,20 @@ def process_knee_scan():
 
     response = requests.get(decoded_url)
     if response.status_code == 200:
-        image = Image.open(BytesIO(response.content))
+        image = Image.open(BytesIO(response.content)).convert("RGB")
         
         try:
-            knee_crop_result = knee_crop_model.predict(image)[0]
-            x1, y1, x2, y2 = map(int, knee_crop_result.boxes[0].xyxy[0].numpy())
+            # knee_crop_result = knee_crop_model.predict(image)[0]
+            # x1, y1, x2, y2 = map(int, knee_crop_result.boxes[0].xyxy[0].numpy())
 
-            boundary = 50
-            x1, y1, x2, y2 = x1 - boundary, y1 - boundary, x2 + boundary, y2 + boundary
-            cropped_scan = image.crop((x1, y1, x2, y2)).convert("RGB")
+            # boundary = 50
+            # x1, y1, x2, y2 = x1 - boundary, y1 - boundary, x2 + boundary, y2 + boundary
+            # cropped_scan = image.crop((x1, y1, x2, y2)).convert("RGB")
 
-            transform = transforms.Compose([
-                transforms.Resize((640, 640)),  
-                transforms.ToTensor(),          
-            ])
-        
-            cropped_tensor = transform(cropped_scan).unsqueeze(0)  
-            
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            cropped_tensor = cropped_tensor.to(device)
+            final_result = predict_classes(image)
+            print(final_result)
 
-            final_results = knee_classification_model.predict(cropped_tensor)[0]
-
-            final_predictions = list(map(lambda val: round(val, 3), final_results.probs.data.tolist()))
-            return jsonify({"healthy": final_predictions[0], "doubtful": final_predictions[1], "minimal": final_predictions[2], "moderate": final_predictions[3], "severe": final_predictions[4]}), 200
+            return jsonify({"healthy": round(final_result[0][0], 2), "moderate": round(final_result[0][1], 2), "severe": round(final_result[0][2], 2)})
 
         except Exception as e:
             print(e)
